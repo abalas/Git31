f58f0b7bffe65a6cbeea3369309179115e740bc5
alumno <alumno@1-DAW-19>
2025-03-07 13:10:40 +0100

    Saludo a Juan

---
+++ src/PaquetePruebasGit/PruebaGit.java
@@ -0,0 +1,11 @@
+package PaquetePruebasGit;
+
+public class PruebaGit {
+
+	public static void main(String[] args) {
+		System.out.println("Hola Mundo");
+		System.out.println("Hola Juan");
+
+	}
+
+}
